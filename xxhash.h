f3aa4c534d3ab85c0898fd64aba22d8289ac56a7
Steven Noonan <steven@uplinklabs.net>
2021-12-19 06:30:30 -0800

    allow defining custom malloc/free functions
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- xxhash.h
+++ xxhash.h
@@ -1693,6 +1693,7 @@ XXH3_128bits_reset_withSecretandSeed(XXH3_state_t* statePtr,
 /* *************************************
 *  Includes & Memory related functions
 ***************************************/
+#if !defined(XXH_CUSTOM_MALLOC)
 #if defined(XXH_NO_STREAM)
 /* nothing */
 #elif defined(XXH_NO_STDLIB)
@@ -1730,6 +1731,7 @@ static XXH_MALLOCF void* XXH_malloc(size_t s) { return malloc(s); }
 static void XXH_free(void* p) { free(p); }
 
 #endif  /* XXH_NO_STDLIB */
+#endif  /* !XXH_CUSTOM_MALLOC */
 
 #include <string.h>
 
