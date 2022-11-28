f28b1bfc4364929655c7cbb48f8180a938f6383a
Steven Noonan <steven@uplinklabs.net>
2022-11-28 08:58:36 -0800

    xxh_x86dispatch: use correct XXH_accumulate_512 for dispatch
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- xxh_x86dispatch.c
+++ xxh_x86dispatch.c
@@ -527,7 +527,8 @@ XXHL64_default_##suffix(XXH_NOESCAPE const void* XXH_RESTRICT input,          \
 {                                                                             \
     return XXH3_hashLong_64b_internal(                                        \
                input, len, XXH3_kSecret, sizeof(XXH3_kSecret),                \
-               XXH3_accumulate_##suffix, XXH3_scrambleAcc_##suffix            \
+               XXH3_accumulate_##suffix, XXH3_accumulate_512_##suffix,        \
+               XXH3_scrambleAcc_##suffix                                      \
     );                                                                        \
 }                                                                             \
                                                                               \
@@ -539,7 +540,8 @@ XXHL64_seed_##suffix(XXH_NOESCAPE const void* XXH_RESTRICT input, size_t len, \
 {                                                                             \
     return XXH3_hashLong_64b_withSeed_internal(                               \
                     input, len, seed, XXH3_accumulate_##suffix,               \
-                    XXH3_scrambleAcc_##suffix, XXH3_initCustomSecret_##suffix \
+                    XXH3_accumulate_512_##suffix, XXH3_scrambleAcc_##suffix,  \
+                    XXH3_initCustomSecret_##suffix                            \
     );                                                                        \
 }                                                                             \
                                                                               \
@@ -552,7 +554,8 @@ XXHL64_secret_##suffix(XXH_NOESCAPE const void* XXH_RESTRICT input,           \
 {                                                                             \
     return XXH3_hashLong_64b_internal(                                        \
                     input, len, secret, secretLen,                            \
-                    XXH3_accumulate_##suffix, XXH3_scrambleAcc_##suffix       \
+                    XXH3_accumulate_##suffix, XXH3_accumulate_512_##suffix,   \
+                    XXH3_scrambleAcc_##suffix                                 \
     );                                                                        \
 }                                                                             \
                                                                               \
@@ -574,7 +577,8 @@ XXHL128_default_##suffix(XXH_NOESCAPE  const void* XXH_RESTRICT input,        \
 {                                                                             \
     return XXH3_hashLong_128b_internal(                                       \
                     input, len, XXH3_kSecret, sizeof(XXH3_kSecret),           \
-                    XXH3_accumulate_##suffix, XXH3_scrambleAcc_##suffix       \
+                    XXH3_accumulate_##suffix, XXH3_accumulate_512_##suffix,   \
+                    XXH3_scrambleAcc_##suffix                                 \
     );                                                                        \
 }                                                                             \
                                                                               \
@@ -588,7 +592,8 @@ XXHL128_secret_##suffix(XXH_NOESCAPE const void* XXH_RESTRICT input,          \
 {                                                                             \
     return XXH3_hashLong_128b_internal(                                       \
                     input, len, (const xxh_u8*)secret, secretLen,             \
-                    XXH3_accumulate_##suffix, XXH3_scrambleAcc_##suffix);     \
+                    XXH3_accumulate_##suffix, XXH3_accumulate_512_##suffix,   \
+                    XXH3_scrambleAcc_##suffix);                               \
 }                                                                             \
                                                                               \
 /* ===   XXH128 Seeded variants   === */                                      \
@@ -598,7 +603,8 @@ XXHL128_seed_##suffix(XXH_NOESCAPE const void* XXH_RESTRICT input, size_t len,\
                       XXH64_hash_t seed)                                      \
 {                                                                             \
     return XXH3_hashLong_128b_withSeed_internal(input, len, seed,             \
-                    XXH3_accumulate_##suffix, XXH3_scrambleAcc_##suffix,      \
+                    XXH3_accumulate_##suffix, XXH3_accumulate_512_##suffix,   \
+                    XXH3_scrambleAcc_##suffix,                                \
                     XXH3_initCustomSecret_##suffix);                          \
 }
 
